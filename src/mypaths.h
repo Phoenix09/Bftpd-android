0545e5dd355d5599fd61980a664a6fbe68413a28
Phoenix09 <Phoenix09@users.noreply.github.com>
2016-08-24 13:41:00 +0100

    mypaths.h: Update paths for Android

--- src/mypaths.h
+++ src/mypaths.h
@@ -1,5 +1,7 @@
-#define PATH_BFTPD_CONF "/etc/bftpd.conf"
+#define PATH_BFTPD_CONF "/data/local/bftpd/bftpd.conf"
 #ifdef PREFIX
-#define PATH_BFTPD_CONF_WITH_PREFIX PREFIX"/etc/bftpd.conf"
+#define PATH_BFTPD_CONF_WITH_PREFIX PREFIX"/data/local/bftpd/bftpd.conf"
 #endif
 #define PATH_STATUSLOG "/dev/null"
+#undef HAVE_UTMP_H
+#undef HAVE_SHADOW_H
